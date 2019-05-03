4e759d4d8e11c42a9e7a6d2b77fc48562cbc63ca
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-05-03 14:50:09 -0400

    added initial table values

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,16 @@ stock_quantity INT(10) NOT NULL,
 PRIMARY KEY (id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Cheez-its", "Food", 1.10, 500),
+("HDMI Cable", "Electronics", 30, 50),
+("Goose Eggs", "Food", 2.00, 1000),
+("Floppy Discs - Bulk", "Electronics", .10, 5000),
+("Frozen Turkey", "Food", 35, 1),
+("DVD Player", "Electronics", 12.50, 40),
+("Left Shoes", "Clothing", 5.00, 500),
+("Right Shoes", "Clothing", 5.10, 501),
+("Left Twix", "Food", 0.10, 500),
+("Right Twix", "Food", 0.10, 500)
+;
